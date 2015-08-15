dbb5361db86afacb7cbb44561452ca62925ea0f6
Your Name <you@example.com>
2015-08-15 17:00:29 +0800

    Keep only openweather part

--- scripts/apps/main.js
+++ scripts/apps/main.js
@@ -1,16 +1,6 @@
 define(["lib/react", "lib/jquery"], function(React, jquery){
 	var Weather = React.createClass({
 		render: function(){
-			//var weatherUrl = "http://www.weather.com.cn/data/cityinfo/101200101.html"
-			// $.ajax({
-			// 	async: false,
-			// 	url: "http://www.weather.com.cn/data/cityinfo/101200101.html",
-			// 	type: "GET",
-			// 	dataType: "jsonp",
-			// 	sucess: function(jsonp){
-			// 		console.log(jsonp);
-			// 	}
-			// })
 			var openWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=beijing,cn&callback=?"
 			$.getJSON(openWeatherUrl, function(json){
 				console.log(json);
