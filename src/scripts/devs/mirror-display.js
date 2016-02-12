65954260e2fac7eb7e7314bf75d0d3d072b981e6
Yansen <Yansen@test.com>
2016-02-12 12:29:02 +0800

    Get position from stat

--- src/scripts/devs/mirror-display.js
+++ src/scripts/devs/mirror-display.js
@@ -1,27 +1,45 @@
 define(["react", "lib/react-draggable", "apps/date-tile", "apps/weather-tile", "apps/praise-message-tile"], 
 	function(React, Draggable, DateTile, WeatherTile, PraiseMessageTile){
 		var MirrorDisplay = React.createClass({
+		 	getInitialState: function(){
+				return {tilePositions: 
+									{
+										"dateTile":{left:50, top:50}, 
+										"weatherTile":{left:1000, top:50}, 
+										"praiseMessageTile":{left:300, top:500}
+									}
+								};
+			},
+			componentDidMount: function(){
+				// Get Position from Server	
+			},
+			handleDateTileDragStop: function(event, ui){
+				this.updateTilePosition("dateTile", ui.position);
+			},
+			updateTilePosition: function(tileName, newPosition){
+					// Post to Server to save
+			},
 			render: function(){
 				var dateTileStyle = {
 					position: "fixed",
-					top: 50,
-					left: 50
+					top: this.state.tilePositions.dateTile.top,
+					left: this.state.tilePositions.dateTile.left
 				};
 				var weatherTileStyle = {
 					position: "fixed",
 					float: "right",
-					top: 50,
-					right: 50
+					top: this.state.tilePositions.weatherTile.top,
+					left: this.state.tilePositions.weatherTile.left
 				};
 				var praiseMessageTileStyle = {
 					position: "fixed",
-					bottom: 20,
-					right: "50%"
+					top: this.state.tilePositions.praiseMessageTile.top,
+					left: this.state.tilePositions.praiseMessageTile.left
 				};
 
 				return (
 					<div>
-						<Draggable>
+						<Draggable onStop={this.handleDateTileDragStop}>
 							<div style={dateTileStyle}>
 								<DateTile />
 							</div>
